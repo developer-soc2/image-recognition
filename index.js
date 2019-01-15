6b09fdaecd84341c1c4797304dfc9ff406e12e79
developer-soc2 <developer.soc2@gmail.com>
2019-01-15 20:30:34 +0800

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require("express");//to import the express library
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var app = express();
+
+app.get("/test", function (req, res) {
+    var visualRecognition = new VisualRecognitionV3({
+        version: '2018-03-19',
+        iam_apikey: 'Ae8wfpNwYI-OU88zzvem1L7iH0LzfUxdK1SElGV5VZQa'
+    });
+
+    var params = {
+        url: "https://www.t-mobile.com/content/dam/t-mobile/en-p/cell-phones/apple/apple-iphone-x/silver/Apple-iPhoneX-Silver-1-3x.jpg"
+    };
+
+    visualRecognition.classify(params, function (err, response) {
+        if (err)
+            console.log(err);
+        else{
+            //Store the response into a string
+            var result = JSON.stringify(response, null, 2);
+            res.end(result);
+            console.log(result);
+            
+        }
+    });
+
+})
+
+    //var listener = app.listen(4000, process.env.IP, function () {
+    var listener = app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("server has started");
+    console.log('Listening on port ' + listener.address().port);
+});
